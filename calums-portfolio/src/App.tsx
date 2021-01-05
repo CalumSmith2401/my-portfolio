6d65eb262e98586dc57b9b3057ce401784cd0cc7
unknown <CalumSmith2401@gmail.com>
2021-01-05 18:10:49 +0800

    First commit

---
+++ calums-portfolio/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {
+	H1,
+	P
+} from "./styles";
+
+
+function App() {
+  return (    
+    <div className="App">      
+      <header> 
+        <H1>Calum's Portfolio</H1>
+        <P>Hi my name is Calum, I have been working with Climate Clever as an Intern for about a year
+        while completing a diploma at TAFE. Over my programming journey i have learnt some Java, C#
+        HTML, CSS and JavaScript(React).
+        </P>  
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ calums-portfolio/src/styles.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const H1 = styled.h1(() => ({
+    color: "red",
+	position: "relative",
+}));
+
+export const P = styled.p(() => ({
+    color: "blue",
+	position: "relative",
+}));
