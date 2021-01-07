3494fdb53eedfc4a51a13eaab25899ae7c2f9480
unknown <CalumSmith2401@gmail.com>
2021-01-07 10:33:46 +0800

    Added a footer

--- calums-portfolio/src/App.tsx
+++ calums-portfolio/src/App.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import './App.css';
-import {
-	H1,
-	P
-} from "./styles";
-
+import React from "react";
+import "./App.css";
+import { H1, P, FOOTER } from "./styles";
 
 function App() {
-  return (    
-    <div className="App">      
-      <header> 
+  return (
+    <div className="App">
+      <header>
         <H1>Calum's Portfolio</H1>
-        <P>Hi my name is Calum, I have been working with Climate Clever as an Intern for about a year
-        while completing a diploma at TAFE. Over my programming journey i have learnt some Java, C#
-        HTML, CSS and JavaScript(React).
-        </P>  
+        <FOOTER>
+          <ul>
+            <a href="https://github.com/CalumSmith2401">GitHub </a>
+            <a href="https://www.linkedin.com/in/calum-smith-86a535203/">
+              LinkedIn
+            </a>
+          </ul>
+        </FOOTER>
+        <P>
+          Hi my name is Calum, I have been working with Climate Clever as an
+          Intern for about a year while completing a diploma at TAFE. Over my
+          programming journey i have learnt some Java, C# HTML, CSS and
+          JavaScript(React).
+        </P>
       </header>
     </div>
   );
--- calums-portfolio/src/styles.ts
+++ calums-portfolio/src/styles.ts
@@ -1,11 +1,13 @@
 import styled from "@emotion/styled";
 
 export const H1 = styled.h1(() => ({
-    color: "red",
-	position: "relative",
+  color: "red",
+  position: "relative",
 }));
 
 export const P = styled.p(() => ({
-    color: "blue",
-	position: "relative",
+  color: "blue",
+  position: "relative",
 }));
+
+export const FOOTER = styled.footer(() => ({}));
