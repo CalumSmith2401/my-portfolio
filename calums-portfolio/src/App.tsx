2dc77264db6ef101cd62d22fa94b19e038e1d246
CalumSmith2401 <60100832+CalumSmith2401@users.noreply.github.com>
2021-01-12 10:12:09 +0800

    Edited Footer

--- calums-portfolio/src/App.tsx
+++ calums-portfolio/src/App.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 import "./App.css";
+import profile from "./myProfile.jpeg";
 import { H1, P, FOOTER } from "./styles";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <H1>Calum's Portfolio</H1>
+      <header className="App-header">
+          <H1>Calum's Portfolio</H1>
+          <P>
+            Hi my name is Calum, I have been working with Climate Clever as an
+            Intern for about a year while completing a diploma at TAFE. Over my
+            programming journey I have learnt some Java, C#, HTML, PHP, CSS and
+            JavaScript(React).
+          </P>
+          <br></br>
+          <P>
+            Projects:
+          </P>
         <FOOTER>
-          <ul>
             <a href="https://github.com/CalumSmith2401">GitHub </a>
             <a href="https://www.linkedin.com/in/calum-smith-86a535203/">
               LinkedIn
             </a>
-          </ul>
         </FOOTER>
-        <P>
-          Hi my name is Calum, I have been working with Climate Clever as an
-          Intern for about a year while completing a diploma at TAFE. Over my
-          programming journey i have learnt some Java, C# HTML, CSS and
-          JavaScript(React).
-        </P>
       </header>
     </div>
   );
--- calums-portfolio/src/styles.ts
+++ calums-portfolio/src/styles.ts
@@ -6,8 +6,22 @@ export const H1 = styled.h1(() => ({
 }));
 
 export const P = styled.p(() => ({
-  color: "blue",
+  color: "white",
   position: "relative",
+  bottom: 0
 }));
 
-export const FOOTER = styled.footer(() => ({}));
+export const FOOTER = styled.footer(() => ({
+  position: "absolute",
+  bottom: 0,
+  width: "100%",
+  height: "2.5rem",
+  color: "white"
+}));
+
+export const APPHEADER = styled.header(() => ({
+    backgroundColor: "#282c34",
+    minHeight: "100vh",
+    fontSize: "calc(10px + 2vmin)",
+    color: "white"
+}));
