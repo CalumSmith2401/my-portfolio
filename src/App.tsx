6172a37b41a117bd5d205d8a9591fd0c7b73bbc5
CalumSmith2401 <60100832+CalumSmith2401@users.noreply.github.com>
2021-01-12 21:45:24 +0800

    Added avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./App.css";
 import { H1, P, FOOTER } from "./styles";
+import avatar from "./AvatarMaker.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <H1>Calum's Portfolio</H1>
-          <P>
-            Hi my name is Calum, I have been working with Climate Clever as an
-            Intern for about a year while completing a diploma at TAFE. Over my
-            programming journey I have learnt some Java, C#, HTML, PHP, CSS and
-            JavaScript(React).
-          </P>
-          <br></br>
-          <P>
-            Projects:
-          </P>
+        <H1>Calum's Portfolio</H1>
+        <img src={avatar} width="200px" height="200px" />
+        <P>
+          Hi my name is Calum, I have been working with Climate Clever as an
+          Intern for about a year while completing a diploma at TAFE. Over my
+          programming journey I have learnt some Java, C#, HTML, PHP, CSS and
+          JavaScript(React).
+        </P>
+        <br></br>
+        <P>Projects:</P>
         <FOOTER>
-            <a href="https://github.com/CalumSmith2401">GitHub </a>
-            <a href="https://www.linkedin.com/in/calum-smith-86a535203/">
-              LinkedIn
-            </a>
+          <a href="https://github.com/CalumSmith2401">GitHub </a>
+          <a href="https://www.linkedin.com/in/calum-smith-86a535203/">
+            LinkedIn
+          </a>
         </FOOTER>
       </header>
     </div>
